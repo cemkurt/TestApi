15be25d8f24b2f46af047b22fee4120a0bc255ba
cem-kurt <cem.kurt@path.com.tr>
2022-02-09 15:09:42 +0300

    asd

--- Api/Program.cs
+++ Api/Program.cs
@@ -14,14 +14,15 @@ namespace Api
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://*:1009");
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5001/");
                 });
     }
 }
